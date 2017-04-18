c4b89a3930b9ee3ea9fcf3cf11915d27383de428
rain <rain2288@gmail.com>
2017-04-18 16:39:41 -0700

    Updated Reply button to redirect to "Confirmation" after submit edited Test data to Fake values

--- application/views/pages/ViewMessage.php
+++ application/views/pages/ViewMessage.php
@@ -1,6 +1,6 @@
 <?php
-$item = array('name' => 'TESTNAME', 'description' => 'Descritipiont asdfsadf', 'category_id' => 'Tech', 'price' => '4.99');
-$message = array('sender_name' => 'John', 'receiver_name' => 'Jason', 'date_sent' => '2017-04-17 22:58:33', 'message' => "hello blah balh bah", 'location_id' => "Quad");
+$item = array('name' => 'Harry Potter Book', 'description' => 'Descritipiont asdfsadf', 'category_id' => 'Book', 'price' => '4.99');
+$message = array('sender_name' => 'John Smith', 'receiver_name' => 'Jason', 'date_sent' => '2017-04-17 22:58:33', 'message' => "hello, i wanted to see if you can meet me at Quad.", 'location_id' => "Quad");
 ?>
 <div class="container-fluid">
     <div class="row">
@@ -21,35 +21,38 @@ $message = array('sender_name' => 'John', 'receiver_name' => 'Jason', 'date_sent
             ?>
         </div>
     </div>
-    <div class = "row">
-        <div class ="col-md-2"></div>
-        <div class ="col-md-8">
-            <div class="center-block">
-                <?php
-                echo "<h3>From: $message[sender_name] @ $message[date_sent]</h3>";
-                echo "<pre>$message[message]</pre>";
-                ?>
-            </div>
+    <form method="post" id="ViewMessage" action ="<?= site_url("Confirmation"); ?>">
 
-            <form>
+        <div class = "row">
+            <div class ="col-md-2"></div>
+            <div class ="col-md-8">
+                <div class="center-block">
+                    <?php
+                    echo "<h3>From: $message[sender_name] @ $message[date_sent]</h3>";
+                    echo "<pre>$message[message]</pre>";
+                    ?>
+                </div>
+
+                
                 <div class="form-group">
                     <label for="reply">Reply:</label>
                     <textarea class="form-control" rows="5" id="description" name = "reply"></textarea>
                 </div>
-            </form>
+
+            </div>
+            <div class ="col-md-2"></div>
 
         </div>
-        <div class ="col-md-2"></div>
 
-    </div>
+        <div class = "row">
+            <div class = "col-md-5"> </div>
+            <div class = "col-md-2"> 
+                <a href="<?= site_url("ViewMessages"); ?>" class="btn btn-default btn-block" role="button">Back</a>
 
-    <div class = "row">
-        <div class = "col-md-5"> </div>
-        <div class = "col-md-2"> 
-            <a href="<?= site_url("ViewMessages"); ?>" class="btn btn-default btn-block" role="button">Back</a>
-
-            <a href="<?= site_url(); ?>" button type="submit" class="btn btn-success btn-block"  role="button">Reply</a>
+                <a href="<?= site_url(); ?>" button type="submit" class="btn btn-success btn-block"  role="button">Reply</a>
+            </div>
+            <div class = "col-md-5"> </div>
         </div>
-        <div class = "col-md-5"> </div>
-    </div>
+    </form>
+
 </div>
