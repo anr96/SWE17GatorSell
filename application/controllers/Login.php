62db66f82eb07cc9f5adc165c6fafd3d5a0308b0
Ronald Rieger <rrieger@email.sfsu.edu>
2017-04-30 20:09:08 -0700

    implemented form validation on login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,19 +1,23 @@
 <?php
 
 class Login extends CI_Controller {
-    public $registered_user = false;
+    public $registered_user;
 
     public function index() {
         must_not_be_logged_in();
         
-        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|regex_match[/.*sfsu.edu/]|strtolower');
-        $this->form_validation->set_rules('password', 'Password', 'required|callback_validate_user');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|regex_match[/.*sfsu.edu/]|strtolower',array(
+            'regex_match' => 'Not a valid SFSU email address'
+        ));
+        $this->form_validation->set_rules('password', 'Password', 'required|callback_validate_user',array(
+            'validate_user' => 'Invalid email and/or password'
+        ));
         
         if ($this->form_validation->run() == FALSE) {
             gator_view('Login', 'pages/Login');
         } else {
-            $this->session->set_userdata('registered_user', $this->$registered_user);
-            if($this->$registered_user['activated']){
+            $this->session->set_userdata('registered_user', $this->registered_user);
+            if($this->registered_user['activated']){
                 redirect($_SESSION['continue_destination']);
             } else {
                 redirect('register/activate');
@@ -26,12 +30,12 @@ class Login extends CI_Controller {
         redirect($_SESSION['cancel_destination']);
     }
     
-    public function validate_user($param) {
+    public function validate_user($password) {
         $this->load->model('registered_user_accounts');
         $email = $this->input->post('email');
-        $password = $this->input->post('password');
-        $this->$registered_user = $this->registered_user_accounts->validate_email_and_password($email,$password);
+        //$password = $this->input->post('password');
+        $this->registered_user = $this->registered_user_accounts->validate_email_and_password($email,$password);
         
-        return isset($this->$registered_user);
+        return (bool) isset($this->registered_user);
     }
 }
