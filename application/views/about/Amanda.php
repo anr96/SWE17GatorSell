533297e30c192132d0397b49ab1cbc3e4ff1f7dc
anr96 <anikkole@gmail.com>
2017-05-02 23:08:41 -0700

    my about page

--- application/views/about/Amanda.php
+++ application/views/about/Amanda.php
@@ -1,5 +1,5 @@
-<div class="jumbotron">
-    <div class="container">
+<div class="container-fluid">
+    <div class="jumbotron">
         <div class="row">
             <div class="col-sm-4">
                 <img src="<?= base_url('assets/img/amanda_pic.JPG'); ?>" class="img-responsive img-circle">
@@ -8,7 +8,42 @@
                 <H1 class="text-center">Amanda Robinson</H1>
             </div>
         </div>
+        
     </div>
+    <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+            <div class="panel panel-body text-center">
+                <p>I am currently studying computer science and linguistics at San Francisco State University.</p>
+                <p><strong>You're now probably thinking "Wow, those two things really aren't related, right?"</strong> Guess again! </p>        
+                <p>I enjoy all things natural language processing/computational linguistics related and intend to continue 
+                engineering NLP software well past my graduation in May 2018.</p>
+            </div>
+            <div class="panel panel-body ">
+                <div class="col-sm-8">
+                <h4>My current projects and affiliations:</h4>
+                <li>Lead Programmer: English Adjectival Bigrams and Google N-Grams</li>
+                <li>Programmer: Evolution of the Russian Language</li>
+                <li>Co-founder: SFSU English Student Association</li>
+                <li>Co-founder: LeanIn CS & E chapter at SFSU</li>
+                <li>Mentor: Reading Partners in San Leandro</li>
+                </div>
+                <div class="col-sm-4 text-center"></div>
+            </div>
+        </div>
+        
+        <div class="col-sm-2">
+            <center><a href="http://www.linkedin.com/in/anr96"><img src="<?= base_url('assets/img/linkedin.png'); ?>" style="width:100px;height:100px;" class="img-responsive"></a></center>
+            <center><a href="http://www.github.com/anr96"><img src="<?= base_url('assets/img/github.png'); ?>" class="img-responsive"></a></center>
+                    
+        </div>
+    </div>
+    <div class="row">
+        <div class="text-center">
+            <a href="<?= site_url('about'); ?>" class="btn btn-default">Back to the Team</a><p></p>
+        </div>
+    </div>
+    
 </div>
 
 
