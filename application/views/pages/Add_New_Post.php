3f759b33f683a54b4d499c1504ac57bbea195b28
rain <rain2288@gmail.com>
2017-05-03 14:29:00 -0700

    Updated Googlemaps JS, panTo locations

--- application/views/pages/Add_New_Post.php
+++ application/views/pages/Add_New_Post.php
@@ -68,19 +68,73 @@ $locations = array(
 
                     <script>
 
-                        var map;
+//                        var map;
+//                        var marker;
+//                        //var markers = [];
+//
+//                        function initMap() {
+//                            var SFSU = {lat: 37.722558, lng: -122.4780799};
+//                            map = new google.maps.Map(document.getElementById('map'), {
+//                                zoom: 17,
+//                                center: SFSU
+//                            });
+//                            marker = new google.maps.Marker({
+//                                position: SFSU,
+//                                map: map
+//                            });
+//                            //markers.push(marker);
+//                        }
+    var map;
+      var markers = [];
 
-                        function initMap() {
-                            var uluru = {lat: 37.722558, lng: -122.4780799};
-                            map = new google.maps.Map(document.getElementById('map'), {
-                                zoom: 17,
-                                center: uluru
-                            });
-                            marker = new google.maps.Marker({
-                                position: uluru,
-                                map: map
-                            });
-                        }
+      function initMap() {
+        var SFSU = {lat: 37.722558, lng: -122.4780799};
+
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 17,
+          center: SFSU,
+        });
+
+//        // This event listener will call addMarker() when the map is clicked.
+//        map.addListener('click', function(event) {
+//          addMarker(event.latLng);
+//        });
+
+        // Adds a marker at the center of the map.
+        addMarker(SFSU);
+      }
+
+      // Adds a marker to the map and push to the array.
+      function addMarker(location) {
+        var marker = new google.maps.Marker({
+          position: location,
+          map: map
+        });
+        markers.push(marker);
+      }
+
+      // Sets the map on all markers in the array.
+      function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+        }
+      }
+
+      // Removes the markers from the map, but keeps them in the array.
+      function clearMarkers() {
+        setMapOnAll(null);
+      }
+
+      // Shows any markers currently in the array.
+      function showMarkers() {
+        setMapOnAll(map);
+      }
+
+      // Deletes all markers in the array by removing references to them.
+      function deleteMarkers() {
+        clearMarkers();
+        markers = [];
+      }
 
                     </script>
 
