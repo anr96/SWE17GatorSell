47e446f6e8d1962dc2d5027234a75b726bf82d1d
Ronald Rieger <rrieger@email.sfsu.edu>
2017-05-16 13:37:47 -0700

    added google analytics

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,3 +1,23 @@
+// google analytics
+
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-99237301-1', 'auto');
+ga('send', 'pageview');
+
+
+
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -33,16 +53,16 @@ $("#locations").change(function () {
             uluru = {lat: 37.722558, lng: -122.4780799}; //default
             break;
     }
-    
-    //TODO: change markers to array, then delete markers instead of insantiating an new map every call
-            deleteMarkers();
-       // map.setZoom(18);
-        map.panTo(uluru);
-        //setMapOnAll(null);
-        //marker.setMap(null)
-        //markers = [];
 
-addMarker(uluru);
+    //TODO: change markers to array, then delete markers instead of insantiating an new map every call
+    deleteMarkers();
+    // map.setZoom(18);
+    map.panTo(uluru);
+    //setMapOnAll(null);
+    //marker.setMap(null)
+    //markers = [];
+
+    addMarker(uluru);
 
 
 //    marker = new google.maps.Marker({
